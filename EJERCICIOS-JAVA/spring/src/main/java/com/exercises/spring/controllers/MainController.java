2368f85764405b2e2edb064a6afc5886f4bffe10
John Kervin Valle Cabuntasan <johnkvcode31@gmail.com>
2023-07-31 13:25:11 +0200

    I renamed the file and changed the associated endpoint with the new one

--- EJERCICIOS-JAVA/spring/src/main/java/com/exercises/spring/controllers/MainController.java
+++ EJERCICIOS-JAVA/spring/src/main/java/com/exercises/spring/controllers/MainController.java
@@ -1,23 +1,24 @@
 package com.exercises.spring.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping({"/", "/index"})
-    public String index() {
-       return "index.html";
-    }
+  @GetMapping({ "/", "/index" })
+  public String index() {
+    return "index-test.html";
+  }
 
-    @GetMapping("/second")
-    public String secondPage() {
-        return "second-page.html";
-    }
+  @GetMapping("/second")
+  public String secondPage() {
+    return "second-page.html";
+  }
 
-    @GetMapping("/third")
-    public String thirdPage() {
-        return "third-page.html";
-    }
+  @GetMapping("/third")
+  public String thirdPage() {
+    return "third-page.html";
+  }
 
 }
